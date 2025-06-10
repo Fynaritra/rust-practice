852298f20c8a43239365a7b114a5afce15a44988
finaritra.mickaelle@gmail.com <finaritra.mickaelle@gmail.com>
2025-06-10 23:08:18 +0200

    Guess number & fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn fib(n: u32) -> u32 {
     if n < 2 {
@@ -12,4 +15,29 @@ fn fib(n: u32) -> u32 {
 fn main() {
     let n = 20;
     println!("fib({n}) = {}", fib(n));
+    
+    println!("Devinez le nombre !");
+
+    let nombre_secret: u32 = rand::rng().random_range(1..101);
+    println!("Le nombre secret est : {}", nombre_secret);
+
+    loop{
+        println!("Veuillez entrer un nombre.");
+        let mut supposition: String = String::new();
+        io::stdin()
+            .read_line(&mut supposition)
+            .expect("Échec de la lecture de l'entrée utilisateur");
+        let supposition: u32 = supposition.trim().parse().expect("Veuillez entrer un nombre !");
+
+
+        println!("Votre nombre : {}", supposition);
+        match supposition.cmp(&nombre_secret) {
+            Ordering::Less => println!("C'est plus !"),
+            Ordering::Greater => println!("C'est moins !"),
+            Ordering::Equal => {
+                println!("Vous avez gagné !");
+                break;
+            }
+        }
+    }
 }
